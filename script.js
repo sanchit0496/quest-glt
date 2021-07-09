038b8ee70780b128d03d27a2916a165e680c01aa
Sanchit <sanchit0496@gmail.com>
2021-07-09 12:26:06 +0530

    the search feature is working

--- script.js
+++ script.js
@@ -33,15 +33,49 @@ function myDisplay(y){
         <table>`;
     }
     data.innerHTML = output;
+
+
+    //using the search term
+    let term = document.getElementById('searchRental');
+
+    term.addEventListener("keyup", e => {
+        let rental = e.target.value;
+        let newoutput =   `
+        <table>     
+            <th>Number</th>
+            <th>Name</th>
+            <th>Co</th>
+            <th>Availability</th>
+            <th>Need to Repair</th>
+            <th>Durability</th>
+            <th>Mileage</th>
+        `;
+
+        for(let i = 0 ; i < y.length; i++){
+
+            if(y[i].name.toLowerCase().includes(rental)){
+               console.log(y[i].name)
+               newoutput += `
+         
+               <tr>
+               <td>${i+1}</td>
+               <td>${y[i].code}</td>  
+               <td>${y[i].name}</td>
+               <td>${y[i].availability}</td>
+               <td>${y[i].needing_repair}</td>
+               <td>${y[i].durability}</td>
+               <td>${y[i].mileage}</td>
+               </tr>  
+               <table>`;
+            }
+        }
+        data.innerHTML = newoutput;
+
+    })
+
+
 }
  
-//using the search term
-let term = document.getElementById('searchRental');
-
-term.addEventListener("keyup", e => {
-    let rental = e.target.value
-    console.log(rental);
-})
 
 
 
