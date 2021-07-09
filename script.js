c64d4fa735b3c7b31bea23fa591e1796329f52a3
Sanchit <sanchit0496@gmail.com>
2021-07-09 11:30:05 +0530

    adding the table and navbar UI

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-let btn = document.getElementById('getData');
+let data = document.getElementById('data')
 
-btn.addEventListener('click', hello)
+fetch('data.json')
+.then(x => x.json())
+.then(y => myDisplay(y));
 
-function hello(){
-    fetch('data.json')
-    .then(x => x.json())
-    .then(y => console.log(y));
+function myDisplay(y){
+    console.log(y)
+    let len = y.length;
+    let output =   `
+    <table>     
+        <th>Number</th>
+        <th>Name</th>
+        <th>Co</th>
+        <th>Availability</th>
+        <th>Need to Repair</th>
+        <th>Durability</th>
+        <th>Mileage</th>
+    `;
+    for(let i = 0 ; i < y.length; i++){
+        output += `
+         
+        <tr>
+        <td>${i+1}</td>
+        <td>${y[i].code}</td>  
+        <td>${y[i].name}</td>
+        <td>${y[i].availability}</td>
+        <td>${y[i].needing_repair}</td>
+        <td>${y[i].durability}</td>
+        <td>${y[i].mileage}</td>
+        </tr>  
+        <table>`;
+    }
+    data.innerHTML = output;
 }
+ 
 
 
